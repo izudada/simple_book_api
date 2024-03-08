8a5c26d3acc375e172e69d3b9cbeada1c62a1f72
Anthony Udeagbala <tonyudeagbala@gmail.com>
2024-03-08 19:59:50 +0100

    create register endpoint

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
+from rest_framework import response, status
+from django.contrib.auth import authenticate
 
-# Create your views here.
+from authentication.serializers import RegisterSerializer
+
+
+class RegisterAPIView(GenericAPIView):
+    serializer_class = RegisterSerializer
+    authentication_classes = []
+    permission_classes = []
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return response.Response(serializer.data, status.HTTP_201_CREATED)
+        return response.Response(serializer.errors, status.HTTP_200_OK)
+    
