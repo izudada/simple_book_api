71fb61ee0fbd78976b986fa86afd7f71bc36c6aa
Anthony Udeagbala <tonyudeagbala@gmail.com>
2024-03-10 15:42:13 +0100

    create order model

--- order/models.py
+++ order/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from authentication.models import User
+from helpers.models import TrackingModel
+
+
+class Order(TrackingModel, models.Model):
+
+    author = models.CharField(max_length=150)
+    meta_data = models.JSONField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Order for {self.user.username}"
