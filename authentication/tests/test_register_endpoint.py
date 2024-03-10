59171abc09f5db83d2ad384c6d8c240f4f1bc262
Anthony Udeagbala <tonyudeagbala@gmail.com>
2024-03-10 15:12:48 +0100

    test register endpoint

--- authentication/tests/test_register_endpoint.py
+++ authentication/tests/test_register_endpoint.py
@@ -1,10 +1,13 @@
+from django.urls import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status
+
+from authentication.models import User
 
 
 class TestRegisterAPI(APITestCase):
     def setUp(self):
-        self.url = f'/api/v1/register'
+        self.url = reverse('register')
         self.payload = {
             "username": "foobar",
             "first_name": "Foo",
@@ -18,20 +21,28 @@ class TestRegisterAPI(APITestCase):
             Test that an authenticated user hits the endpoint
             the request returns 200
         """
-        self.authenticate()
-
-        #   assertion before update
-        self.assertEqual(self.data.get('price'), 10000)
-        response = self.client.put(self.url, self.payload)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        #   assertion after update
-        self.assertEqual(response.data.get('price'), 12000.0)
+        #   assertion before registring
+        self.assertFalse(User.objects.filter(username=self.payload['username']).exists())
+        response = self.client.post(self.url, self.payload)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        #   assertion after registring
+        self.assertTrue(User.objects.filter(username=self.payload['username']).exists())
         
-    def test_update_book_without_auth_fails(self):
+    def test_update_book_with_existing_email_fails(self):
         """
             Test that an unauthenticated user hits the endpoint
-            the request returns 403
+            the request returns email field error
         """
-        response = self.client.put(self.url, self.payload)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        user = User.objects.create(
+            username="arcane",
+            first_name="John",
+            last_name="Jack",
+            email="arcane@gmail.com",
+            password="password1234@#"
+        )
+        user.save()
+        self.payload['email'] = user.email
+        response = self.client.post(self.url, self.payload)
+        self.assertEqual(response.json()['email'], ['user with this email address already exists.'])
+
 
